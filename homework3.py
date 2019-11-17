184737ba14e9a3f9c0226cfa25c258eb3b563624
Niroop Ramdas Sagar <ramdassa@usc.edu>
2019-11-16 16:36:39 -0800

    KB hashmap design

--- homework3.py
+++ homework3.py
@@ -4,6 +4,27 @@ IMPLICATION = "=>"
 CONJUNCTION = "&"
 NEGATION = "~"
 
+args_seen_list = set()
+count = 0
+def is_variable(arg):
+
+    return isinstance(arg, str) and arg[:1].isalpha() and arg[:1].islower()
+
+def standardize_variable():
+
+    global count
+    count += 1
+    return "x" + str(int(count))
+
+def standardize():
+
+    for arg_idx in range(len(self.args)):
+        if self.is_variable(self.args[arg_idx]):
+            if self.args[arg_idx] not in args_seen_list:
+                args_seen_list.add(self.args[arg_idx])
+            else:
+                self.args[arg_idx] = self.standardize_variable(self.args[arg_idx])
+                args_seen_list.add(self.args[arg_idx])
 class Predicate:
 
     def __init__(self, literal):
@@ -12,10 +33,7 @@ class Predicate:
         self.is_negation = False
 
         self.parse_literal(literal)
-
-        print(f"Predicate : {self.name}")
-        print(f"is_neg : {self.is_negation}")
-        print(f"args : {self.args}")
+        #self.standardize()
 
     def parse_literal(self, literal):
 
@@ -23,9 +41,13 @@ class Predicate:
             self.is_negation = True
             literal = literal.lstrip(NEGATION)
 
-        print(f"Literal : {literal}")
         self.name, args = literal.rstrip(")").split("(")
         self.args = args.split(",")
+
+    def dump_predicate(self):
+        print(f"\nPredicate : {self.name}")
+        print(f"is_neg : {self.is_negation}")
+        print(f"args : {self.args}")
 
 class Sentence:
 
@@ -33,39 +55,99 @@ class Sentence:
         self.predicates = []
         self.premise = []
         self.conclusion = []
+        self.old_vars = []
+        self.new_vars = []
+        self.is_implication = False
 
         self.create_predicates(sentence_str)
 
+        self.convert_to_cnf()
+        self.extract_variables()
+
+
+        self.dump_sentences()
+
+    def dump_sentences(self):
+
+        for pred in self.predicates:
+            pred.dump_predicate()
+
+    def convert_to_cnf(self):
+
+        # Eliminate Implication
+        if self.is_implication:
+            for idx in range(len(self.predicates) -1):
+                if self.predicates[idx].is_negation == True:
+                    self.predicates[idx].is_negation = False
+                else:
+                    self.predicates[idx].is_negation = True
+
+        #distribute and over or??
+
+    def extract_variables(self):
+
+        self.var_map = {}
+
+        for idx in range(len(self.predicates)):
+
+            for arg_id, arg in enumerate(self.predicates[idx].args):
+                #print(f"Arg : {arg}")
+                if is_variable(arg):
+                    if arg in self.var_map:
+                        self.predicates[idx].args[arg_id] = self.var_map[arg]
+                    else:
+                        new_arg = standardize_variable()
+                        self.var_map[self.predicates[idx].args[arg_id]] = new_arg
+                        self.predicates[idx].args[arg_id] = new_arg
+
+
+        
     def create_predicates(self, sentence_str):
 
         if IMPLICATION in sentence_str:
             self.premise, self.conclusion = sentence_str.split(IMPLICATION)
+            self.is_implication = True
         else:
             self.premise = sentence_str
 
-        print(f"Premise : {self.premise}")
-        print(f"conclusion : {self.conclusion}")
-
         for literal in self.premise.split(CONJUNCTION) + [self.conclusion]:
             if literal:
-                print(f"Literal  : {literal}")          
-                self.predicates.append(Predicate(literal.strip()))
-            
-        
+                #print(f"Literal  : {literal}")
+                predicate_obj = Predicate(literal.strip())          
+                self.predicates.append(predicate_obj)
+
+    def parse_predicates(self, predicate_obj, literal):
+
+        self.predicate_obj.name, args = literal.rstrip(")").split("(")
+        self.predicate_obj.args = args.split(",")
+
 
 class KnowledgeBase:
     def __init__(self, sentences, queries):
         self.sentences = []
         self.queries = queries
-        print(self.sentences)
-        print(f"queries : {self.queries}")
+        self.kb_map = {}
 
         self.create_sentences(sentences)
+        self.dump_kb()
 
     def create_sentences(self, sentences):
 
         for sentence_str in sentences:
-            self.sentences.append(Sentence(sentence_str))
+            sentence_obj = Sentence(sentence_str)
+            self.sentences.append(sentence_obj)
+            for predicate in sentence_obj.predicates:
+                if predicate.name in self.kb_map:
+                    if sentence_obj not in self.kb_map[predicate.name]:
+                        self.kb_map[predicate.name].append(sentence_obj)
+                else:
+                    self.kb_map[predicate.name] = [sentence_obj]
+
+    def dump_kb(self):
+        print("----------------Knowledge Base-------------------")
+        print(f"KB : {self.kb_map}")
+        print(f"Queries : {self.queries}")
+        print("-------------------------------------------------")
 
 
 if __name__ == "__main__":
@@ -84,6 +166,5 @@ if __name__ == "__main__":
         for i in range(n_sentences):
             sentences.append(in_list[idx].strip("\n"))
             idx += 1
-            
-            
+                        
     KnowledgeBase(sentences, queries)
